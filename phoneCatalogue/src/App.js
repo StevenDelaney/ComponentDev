fb99668dd7fdcd72475e17f8f117e0cacdeb7a92
unknown <stevendelaney90@hotmail.com>
2017-08-24 10:42:21 +0100

    Skeleton rework

--- phoneCatalogue/src/App.js
+++ phoneCatalogue/src/App.js
@@ -1,62 +1,98 @@
-  import React from 'react';
-  import './App.css' 
+import React from 'react';
+import './App.css' ;
+import _ from 'lodash';
 
-    var SelectBox = React.createClass({
-      render: function(){
+      var SelectBox = React.createClass({
+          handleChange : function(e, type,value) {
+               e.preventDefault();
+               this.props.onUserInput( type,value);
+          },
+          handleTextChange : function(e) {
+                this.handleChange( e, 'search', e.target.value);
+          },
+          handleSortChange : function(e) {
+              this.handleChange(e, 'sort', e.target.value);
+          },
+          render: function(){
+               return (
+                 <div className="col-md-10">
+                 <input type="text" placeholder="Search" 
+                              value={this.props.filterText}
+                              onChange={this.handleTextChange} />
+               Sort by:
+                   <select id="sort" value={this.props.order } 
+                             onChange={this.handleSortChange} >
+                   <option value="name">Alphabetical</option>
+                   <option value="age">Newest</option>
+               </select>
+                 </div>
+                );
+              }
+           });
+
+     var PhoneItem = React.createClass({
+          render: function(){
+               return (
+              <li className="thumbnail phone-listing">
+                <a href={'/phones/' + this.props.phone.id} className="thumb">
+                     <img src={this.props.phone.imageUrl} 
+                     alt={this.props.phone.name} /> </a>
+                <a href={'/phones/' + this.props.phone.id}> {this.props.phone.name}</a>
+                <p>{this.props.phone.snippet}</p>
+              </li>
+                ) ;
+             }
+         }) ;
+
+   var FilteredPhoneList = React.createClass({
+        render: function(){
+            var displayedPhones = this.props.phones.map(function(phone) {
+              return <PhoneItem key={phone.id} phone={phone} /> ;
+            }) ;
+            return (
+                    <div className="col-md-10">
+                      <ul className="phones">
+                          {displayedPhones}
+                      </ul>
+                    </div>
+              ) ;
+        }
+    });
+
+var PhoneCatalogueApp = React.createClass({
+     getInitialState: function() {
+           return { search: '', sort: 'name' } ;
+      }, 
+     handleChange : function(type,value) {
+            if ( type === 'search' ) {
+                this.setState( { search: value } ) ;
+              } else {
+                 this.setState( { sort: value } ) ;
+              }
+      }, 
+       render: function(){
+              // console.log('Criteria: Search= ' + this.state.search + 
+                    // ' ; Sort= ' + this.state.sort);
+           var list = this.props.phones.filter(function(p) {
+                  return p.name.toLowerCase().search(
+                         this.state.search.toLowerCase() ) !== -1 ;
+                    }.bind(this) );
+           var filteredList = _.sortBy(list, this.state.sort) ;
            return (
-             <div className="col-md-10">
-            <input type="text" placeholder="Search" />
-            Sort by:
-            <select>
-              <option value="name">Alphabetical</option>
-              <option value="age">Newest</option>
-            </select>
-             </div>
-            );
-          }
-       });
-
-        // TODO (missing component)
-
-       var FilteredPhoneList = React.createClass({
-            render: function(){
-                var displayedPhones = this.props.phones.map(function(phone) {
-                  return <PhoneItem key={phone.id} phone={phone} /> ;
-                }) ;
-                return (
-                        <div className="col-md-10">
-                          <ul className="phones">
-                              {displayedPhones}
-                          </ul>
-                        </div>
-                  ) ;
-            }
-        });
-
-    var PhoneCatalogueApp = React.createClass({
-      render: function(){
-          return (
               <div className="view-container">
               <div className="view-frame">
                  <div className="container-fluid">
                    <div className="row">
-                       <SelectBox />
-                       {   <li class="thumbnail phone-listing">
-            <a href="phones/motorola-xoom-with-wi-fi" 
-                    class="thumb">  
-               <img 
-                 src="assets/images/phones/motorola-xoom-with-wi-fi.0.jpg"
-                 alt="Motorola XOOM\u2122 with Wi-Fi"/> </a>
-            <a href="phones/motorola-xoom-with-wi-fi"> 
-                 Motorola XOOM\u2122 with Wi-Fi </a>
-            <p>The Next, Next Generation\r\n\r\nExperience the future with Motorola XOOM with Wi-Fi, the world's first tablet powered by Android 3.0 (Honeycomb).</p>
-        </li> }
+                      <SelectBox onUserInput={this.handleChange } 
+                             filterText={this.state.search} 
+                             sort={this.state.sort} />
+                       <FilteredPhoneList phones={filteredList} />
                   </div> 
                   </div>                   
                 </div>
               </div>
           );
-      }
-    });
+        }
+});
 
-    export default PhoneCatalogueApp;
+export default PhoneCatalogueApp;
